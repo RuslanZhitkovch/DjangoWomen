826b0b26df2378d0acffcca436b40e24d2c9f24b
Ruslan <rzhitkovich@bk.ru>
2022-12-15 19:09:10 +0300

    Добавил URL в шаблоны

--- website/women/urls.py
+++ website/women/urls.py
@@ -9,7 +9,10 @@ from .views import *
 urlpatterns = [
     path('', index, name = 'home'),
     path('about/',about,name='about'),
-    path('cats/<int:catid>/', categories),
+    path('addpage/', addpage, name = 'add_page'),
+    path('contact/', contact, name = 'contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/',show_post, name = 'post'),
 
 ]
 
--- website/women/views.py
+++ website/women/views.py
@@ -2,15 +2,49 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render,redirect
 from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title' : "Добавить статью", 'url_name': 'add_page'},
+        {'title' : 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name' : 'login'}
+
+
+
+
+
+]
+
+
+
+
 def index(request):
     posts = Women.objects.all()
-    return render(request,'women/index.html', {'posts' : posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+        'posts' : posts,
+        'menu' : menu,
+        'title' : 'Главная страница'
+    }
 
-def categories(request, catid):
-    return HttpResponse(f"Статьи по категориям {catid}")
+    return render(request,'women/index.html', context=context)
+
 
 def about(request):
     return render(request,"women/index.html", {'title': 'О сайте'})
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+def show_post(requst, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
