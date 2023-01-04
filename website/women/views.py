7d7ff0982b9095a851adcd075b8d85001d29e0f2
Ruslan <rzhitkovich@bk.ru>
2023-01-05 00:13:16 +0300

    добавил половину миксинов

--- website/women/views.py
+++ website/women/views.py
@@ -1,52 +1,29 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView
 
 from .forms import *
 from .models import *
-
-
-
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title' : "Добавить статью", 'url_name': 'add_page'},
-        {'title' : 'Обратная связь', 'url_name': 'contact'},
-        {'title': 'Войти', 'url_name' : 'login'}
+from .utils import *
 
 
 
 
-
-]
-
-
-class WomenHome(ListView):
+class WomenHome(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
 
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Главная страница'
-        context['cat_selected'] = 0
-        return context
+        c_def = self.get_user_context(title = "Главная страница")
+        return context | c_def
 
 
     def get_queryset(self):
         return Women.objects.filter(is_published=True)
 
-
-
-# def index(request):
-#     posts = Women.objects.all()
-#     context = {
-#         'posts' : posts,
-#         'menu' : menu,
-#         'title' : 'Главная страница',
-#         'cat_selected' : 0,
-#     }
-#
-#     return render(request,'women/index.html', context=context)
 
 
 def about(request):
@@ -58,52 +35,25 @@ def login(request):
 def contact(request):
     return render(request,"women/feedback.html", {'menu': menu, 'title': 'Обратная связь'})
 
-# def addpage(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             #print(form.cleaned_data)
-#
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = AddPostForm()
-#
-#     return render(request, 'women/addpage.html', {'form':form, 'menu': menu, 'title': 'Добавление статьи'})
-#
-#
 
-
-class AddPage(CreateView):
+class AddPage(DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Добавление статьи'
-        context['menu'] = menu
-
-        return context
+        c_def = self.get_user_context(title = "Добавление статьи")
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Women, slug = post_slug)
-#
-#     context = {
-#         'post': post,
-#         'menu': menu,
-#         'title': post.title,
-#         'cat_selected': 1,
-#     }
-#
-#     return render(request, 'women/post.html', context = context)
 
-
-class ShowPost(DetailView):
+class ShowPost(DataMixin,DetailView):
     model = Women
     template_name = 'women/post.html'
     slug_url_kwarg = 'post_slug'
@@ -111,14 +61,12 @@ class ShowPost(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post']
-        context['menu'] = menu
+        c_def = self.get_user_context(title = context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-        return context
 
-
-class WomenCategory(ListView):
+class WomenCategory(DataMixin,ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
@@ -130,11 +78,10 @@ class WomenCategory(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
 
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
-        context['menu'] = menu
-        context['cat_selected'] = context['posts'][0].cat_id
+        c_def = self.get_user_context(title= 'Категория - ' + str(context['post'][0].cat), cat_selected= context[
+            'posts'][0].cat_id )
 
-        return context
+        return context | c_def
 
 
 
