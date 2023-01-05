71d82e8ae3770e91c2b7d1e85f7a9eed5e793b2b
Ruslan <rzhitkovich@bk.ru>
2023-01-05 16:24:33 +0300

    Создал миксины которые реагируют на авторизацию. Т.е если ты авторизован то у тебя больше ф-ций

--- website/women/views.py
+++ website/women/views.py
@@ -1,25 +1,24 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
 from .models import *
 from .utils import *
 
 
-
-
 class WomenHome(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
-
+    context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title = "Главная страница")
-        return context | c_def
-
+        c_def = self.get_user_context(title="Главная страница")
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Women.objects.filter(is_published=True)
@@ -27,33 +26,37 @@ class WomenHome(DataMixin, ListView):
 
 
 def about(request):
-    return render(request,"women/about.html", {'menu': menu, 'title': 'О сайте'})
-
-def login(request):
-    return HttpResponse("Авторизация")
-
-def contact(request):
-    return render(request,"women/feedback.html", {'menu': menu, 'title': 'Обратная связь'})
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-class AddPage(DataMixin, CreateView):
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
     success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
 
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title = "Добавление статьи")
-        return dict(list(context.items()) + list(c_def.items()))
+        c_def = self.get_user_context(title="Добавление статьи")
+        return context | c_def
 
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-class ShowPost(DataMixin,DetailView):
+
+class ShowPost(DataMixin, DetailView):
     model = Women
     template_name = 'women/post.html'
     slug_url_kwarg = 'post_slug'
@@ -61,44 +64,21 @@ class ShowPost(DataMixin,DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title = context['post'])
+        c_def = self.get_user_context(title=context['post'])
         return dict(list(context.items()) + list(c_def.items()))
 
 
-
-class WomenCategory(DataMixin,ListView):
+class WomenCategory(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
-    allow_empty = False
+
 
     def get_queryset(self):
-        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published = True)
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
     def get_context_data(self, *, object_list=None, **kwargs):
-
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title= 'Категория - ' + str(context['post'][0].cat), cat_selected= context[
-            'posts'][0].cat_id )
-
-        return context | c_def
-
-
-
-
-
-
-# def show_category(request, cat_id):
-#     posts = Women.objects.filter(cat_id = cat_id)
-#
-#     if len(posts) == 0:
-#         raise Http404()
-#
-#     context = {
-#         'posts': posts,
-#         'menu': menu,
-#         'title': 'Отображение по рубрикам',
-#         'cat_selected': cat_id,
-#     }
-#
-#     return render(request, 'women/index.html', context = context)
+        c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat),
+                                      cat_selected=context['posts'][0].cat_id)
+        return dict(list(context.items()) + list(c_def.items()))
